a00151ad2a22d1695461af531404315fe89e836c
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-08 16:35:15 +0700

    add

---
+++ change_label_to_number.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import array
+import sys, csv
+
+# load dataset
+df = pd.read_csv(str(sys.argv[1]), delimiter='\t')
+# Or export it in many ways, e.g. a list of tuples
+tuples = [tuple(x) for x in df.values]
+
+# or export it as a list of dicts
+dicts = df.to_dict().values()
+
+# Create a list to store the data
+label = []
+
+# compare label and dataset
+# For each row in the column,
+for label in df['label']:
+	if label =="normal" :
+		label.append("0")
+	elif label =="malicious" :
+		label.append("1")
+
+# Create a column from the list
+df['label2'] = label
+
+for index, row in df.iterrows():
+	print(row['label2'])
+
+# create column
+# label.append(np.where(df['src_ip']==dfLabel['ip_address'], 'yes', 'no')
+# print(df)
+# df.to_csv("./output/"+str(sys.argv[1]),quoting=csv.QUOTE_ALL)
